6d8dc2015e95b73ec9d6599a36912acc46895059
Erik-Hoffmann <erikhoff.1@web.de>
2022-02-05 21:00:52 +0100

    add bitfields and corresponding tooltips

--- assets/mips_lookup.js
+++ assets/mips_lookup.js
@@ -11,6 +11,8 @@ $(document).ready(() => {
     document.getElementById("mips_search").addEventListener("input", (e) => {
         mips_search(e.srcElement.value);
     });
+
+    tooltip_trigger()
 });
 
 function mips_head() {
@@ -18,8 +20,9 @@ function mips_head() {
     <thead>
         <th>Type</th>
         <th>Instruction</th>
-        <th>RTL</th>
+        <th colspan=6>RTL</th>
         <th>Notes</th>
+        <th></th>
     </thead>
     <tbody></tbody>
     `;
@@ -27,13 +30,62 @@ function mips_head() {
 
 function build_mips_row(el) {
     return `
-    <tr>
-        <td>${el.type}</td>
-        <td>${el.instruction}</td>
-        <td>${el.rtl}</td>
-        <td>${el.notes}</td>
+    <tr class="instruction">
+        <td colspan=1>${el.type}</td>
+        <td colspan=1>${el.instruction}</td>
+        <td colspan=6>${el.rtl}</td>
+        <td colspan=1>${el.notes}</td>
+        <td colspan=1 class="tooltip-toggler"></td>
     </tr>
     `.replace("undefined", "");
+}
+
+function find_by_hash(hash) {
+    var elem
+    mips.forEach(el=>{
+        if (el.instruction.hashCode() === hash) {
+            return elem = el
+        }
+    })
+    return elem
+}
+
+function build_tooltip(el, hash) {
+    return build_type(el.bitfields, hash)
+}
+
+function build_type(fields, hash) {
+    var res_head = `<tr class="bitfields" identifier=${hash}><td colspan=2/>`
+    var res_content = `<tr class="bitfields" identifier=${hash}><td colspan=2/>`
+    var count=6
+    for (key in fields) {
+        var span=1
+        if (key==='const') {
+            span=count
+        }
+        res_head+=`<td colspan=${span}>${key}</td>`
+        res_content+=`<td colspan=${span}>${fields[key]}</td>`
+        count--
+    }
+    res_head+=`<td colspan=2/></tr>`
+    res_content+=`<td colspan=2/></tr>`
+    return res_head+=res_content
+}
+
+function tooltip_trigger() {
+    document.querySelectorAll('.instruction').forEach(item => {
+        item.addEventListener("click", (e) =>{
+            var target = item
+            var identifier = target.children[1].innerHTML.hashCode();
+                if ($(target).hasClass('active')) {
+                    $(target).removeAttr('identifier').toggleClass('active')
+                    $(`[identifier=${identifier}]`).remove()
+                } else {
+                    $(target).attr({"identifier": identifier, class:"active"});
+                    $(build_tooltip(find_by_hash(identifier),identifier)).insertAfter($(target))
+                }
+        })
+    })
 }
 
 function build_mips_table() {
@@ -48,6 +100,7 @@ function mips_search(text) {
     else if (text === "R" || text === "I" || text === "J")
         perform_mips_typeselect(text);
     else perform_mips_search(text);
+    tooltip_trigger()
 }
 
 function drop_mips_table() {
@@ -65,9 +118,9 @@ function perform_mips_typeselect(type) {
 function perform_mips_search(text) {
     mips.forEach((element) => {
         if (
-            element.instruction.includes(text) ||
-            element.rtl.includes(text) ||
-            element.notes?.includes(text)
+            element.instruction.toLowerCase().includes(text.toLowerCase()) ||
+            element.rtl.toLowerCase().includes(text.toLowerCase()) ||
+            element.notes?.toLowerCase().includes(text.toLowerCase())
         )
             instructions.append(build_mips_row(element));
     });
--- assets/script.js
+++ assets/script.js
@@ -58,3 +58,17 @@ function build_formats_row(row) {
     }
     return the_row;
 }
+
+//string.hashCode()
+String.prototype.hashCode = function () {
+    var hash = 0,
+        i,
+        chr;
+    if (this.length === 0) return hash;
+    for (i = 0; i < this.length; i++) {
+        chr = this.charCodeAt(i);
+        hash = (hash << 5) - hash + chr;
+        hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+};
